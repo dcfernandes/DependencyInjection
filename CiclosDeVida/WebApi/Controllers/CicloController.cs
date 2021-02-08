8584d06a6498e3eb7c31fa3fc1d4460fbdd19c0c
DIOGO CARVALHO FERNANDES <dcfernandes@icatuseguros.com.br>
2021-02-08 10:41:30 -0300

    Init

---
+++ CiclosDeVida/WebApi/Controllers/CicloController.cs
@@ -0,0 +1,46 @@
+﻿// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Text;
+    using WebApi.Example;
+
+    [Route("ciclo-vida")]
+    [ApiController]
+    public class CicloController : ControllerBase
+    {
+        private readonly IService _service;
+
+        private readonly ICicloVidaSingleton _singleton;
+        private readonly ICicloVidaScoped _scoped;
+        private readonly ICicloVidaTransient _transient;
+
+        public CicloController(IService service, ICicloVidaSingleton singleton, ICicloVidaScoped scoped, ICicloVidaTransient transient)
+        {
+            _service = service;
+            _singleton = singleton;
+            _scoped = scoped;
+            _transient = transient;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Origem              |Ciclo de Vida  |Id        |DataHora       |Requests");
+            result.AppendLine(_transient.Retorno("Controller     "));
+            result.AppendLine(_service.RetornoTransient());
+
+            result.AppendLine(_scoped.Retorno("Controller     "));
+            result.AppendLine(_service.RetornoScoped());
+
+            result.AppendLine(_singleton.Retorno("Controller     "));
+            result.AppendLine(_service.RetornoSingleton());
+
+            return Ok(result.ToString());
+        }
+    }
+}
